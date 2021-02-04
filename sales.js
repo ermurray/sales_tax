3476a4f88882b8fda6f9db0be00e367ba8017b00
ermurray <ermurray31@gmail.com>
2021-02-04 04:43:21 +0000

    added logic for object in sales data having duplicate value for key name.

--- sales.js
+++ sales.js
@@ -21,12 +21,12 @@ const companySalesData = [
     sales: [ 500, 100 ]
   }
 ];
-
+//calc tax
 const calculateTax = function(sales, taxRate) {
   let taxes = sales * taxRate;
   return taxes;
 };
-
+//calc total sales of given array of sales
 const totalOfSales = function(arrayOfSales) {
   let total = 0;
   for (const sale of arrayOfSales) {
@@ -34,17 +34,36 @@ const totalOfSales = function(arrayOfSales) {
   }
   return total;
 };
-
+// iterate through company objects create new object with company name as key and add pertinent sales info as object for value of that key.
 const calculateSalesTax = function(salesData, taxRates) {
   let result = {};
   for (const obj in salesData) {
-    console.log(salesData[obj]);
-    result[salesData[obj].name] = {
-      'totalSales': totalOfSales(salesData[obj].sales),
-      'totalTaxes': calculateTax(totalOfSales(salesData[obj].sales), salesTaxRates.AB)
-    };
+    // console.log(salesData[obj]);
+    if (result[salesData[obj].name] !== undefined) {
+      result[salesData[obj].name].totalSales += totalOfSales(salesData[obj].sales);
+      result[salesData[obj].name].totalTaxes += calculateTax(totalOfSales(salesData[obj].sales), salesTaxRates.AB);
+    } else {
+      result[salesData[obj].name] = {
+        'totalSales': totalOfSales(salesData[obj].sales),
+        'totalTaxes': calculateTax(totalOfSales(salesData[obj].sales), salesTaxRates.AB)
+      };
+    }
   }
   return result;
 };
 
 console.log(calculateSalesTax(companySalesData, salesTaxRates));
+
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300,
+    totalTaxes: 144
+  }
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
